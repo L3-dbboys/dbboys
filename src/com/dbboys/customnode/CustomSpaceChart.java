1c33f17d178f18c3cb09b58ca02bbb21f5f150a5
L3-dbboys <liufanliang115@163.com>
2026-04-12 19:35:08 +0800

    20260412

--- src/com/dbboys/customnode/CustomSpaceChart.java
+++ src/com/dbboys/customnode/CustomSpaceChart.java
@@ -24,7 +24,7 @@ import java.util.*;
 public class CustomSpaceChart extends BarChart<Number, String> {
 
     /** 水平条单行高度（Oracle / GBase / Informix 容量图统一）。 */
-    public static final int CAPACITY_BAR_ROW_HEIGHT_PX = 15;
+    public static final int CAPACITY_BAR_ROW_HEIGHT_PX = 10;
     /** 同一分类内多段条之间的间隙。 */
     public static final int CAPACITY_CHART_BAR_GAP = 6;
     /** 分类（每一行空间名）之间的间隙。 */
@@ -303,8 +303,8 @@ public class CustomSpaceChart extends BarChart<Number, String> {
         updateXAxis(data);
         updateYAxis(data);
         series.getData().clear();
-        int barHeight = CAPACITY_BAR_ROW_HEIGHT_PX+CAPACITY_CHART_CATEGORY_GAP;
-        int chartHeight = data.size() * barHeight + 52;
+        //int barHeight = CAPACITY_BAR_ROW_HEIGHT_PX+CAPACITY_CHART_CATEGORY_GAP;
+        int chartHeight = data.size() * CAPACITY_BAR_ROW_HEIGHT_PX+(data.size()-1) * CAPACITY_CHART_CATEGORY_GAP + 68;
         setPrefHeight(chartHeight);
         setMinHeight(chartHeight);
 
